fd34cfaf7d39e12b37819c2238817324253a5ab1
AC-0636 <33075704+AC-0636@users.noreply.github.com>
2018-11-26 11:10:14 -0400

    u

--- Phase_1.m
+++ Phase_1.m
@@ -6,7 +6,7 @@ clear; close all; clc;
 %% parameters
 % the time to run before the this scripte stops by itself
 %          [day hour minute seconds]
-run_time = [0   10    0      0];
+run_time = [0   11    0      0];
 time_check = 200;
 alg_size = 1000;
 eFactor = 1250;
